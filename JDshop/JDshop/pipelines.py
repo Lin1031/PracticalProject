e5feaf8b536234ef66ac07de689db8b3053d5836
LinJH1031 <LinJH1031@outlook.com>
2021-04-27 17:43:49 +0800

    feat: connect to database

--- JDshop/JDshop/pipelines.py
+++ JDshop/JDshop/pipelines.py
@@ -9,4 +9,27 @@ import pymysql
 from itemadapter import ItemAdapter
 
 class JdshopPipeline:
-    pass
+
+    def __init__(self):
+        self.connect = pymysql.connect(  # 连接数据库
+            host="159.75.112.216",
+            user="root",
+            password="LinJH123456...",
+            database="JDshop",
+            charset="utf8")
+
+        self.cursor = self.connect.cursor()
+
+    def process_item(self, item, spider):
+        table = 'jd'
+
+        keys = ', '.join(item.keys())
+        values = ', '.join(['%s'] * len(item))
+        sql = 'INSERT INTO {table}({keys}) VALUES ({values})'.format(table=table, keys=keys, values=values)
+        # print(sql)
+        try:
+            self.cursor.execute(sql, tuple(item.values()))
+            self.connect.commit()
+        except:
+            self.connect.ping(True)
+        return item
